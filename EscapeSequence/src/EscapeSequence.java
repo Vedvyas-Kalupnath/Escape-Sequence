f3c46c13ff254d70403ae1da9a782346aad599a9
Asus <Asus@DESKTOP-D9Q8J6U>
2019-08-27 20:40:08 +0400

    Escape Sequence

---
+++ EscapeSequence/src/EscapeSequence.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+
+/**
+ * @author Asus
+ *
+ */
+public class EscapeSequence {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I said \"Hi\" to her");
+		System.out.print("one\ntwo\nthree\n");
+		System.out.print("this\nis\nanother\nexample\n.\n");
+		
+		System.out.print("Read the file \"c:\\windows\\readme.txt\"");
+
+	}
+
+}
